d9f9504e905ce2e25c2f377cbea7931101dca46a
Marble <mrivera0014@gmail.com>
2021-03-16 14:02:12 -0600

    got app to run on localhost, fixed server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
-const db = require('/models');
+// const db = require('/models');
 
 const app = express();
 
@@ -15,6 +15,9 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/populate', { useNewIrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewIrlParser: true, useFindAndModify: false });
 
+app.listen(PORT, () =>
+    console.log(`Server listening on: http://localhost:${PORT}`)
+);
 //Routes will go here
