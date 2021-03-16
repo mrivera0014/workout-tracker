03f363ef47bd5c500519a8cfccbe90f292ba8414
Marble <mrivera0014@gmail.com>
2021-03-16 16:53:43 -0600

    fixed port

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const Workout = require('../models/workout');
 
-router.post('api/workout', ({ body }, res) => {
-    Workout.create(body)
+router.post('/api/workouts', ({ body }, res) => {
+    Workout.create({})
         .then(dbWorkout => {
             res.json(dbWorkout)
         })
@@ -11,4 +11,61 @@ router.post('api/workout', ({ body }, res) => {
         })
 })
 
+router.put('/api/workouts/:id', ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id, { $push: { exercises: body } }, { new: true, runValidators: true }
+    )
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
+})
+
+router.get('/api/workouts', (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
+            }
+        }
+    ])
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
+})
+
+router.get('/api/workouts/range', (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
+            }
+        }
+    ]).sort({ _id: -1 }).limit(7)
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
+})
+
+router.delete('/api/workouts', ({ body }, res) => {
+    Workout.findByIdAndDelete(body.id)
+        .then(() => {
+            res.json(true)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
+})
 module.exports = router
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // const db = require('/models');
 
